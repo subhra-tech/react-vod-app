be8dca0d2903a2a15911e253aca54448e53c2158
Subhra <subhra.306@gmail.com>
2019-04-14 22:22:49 +1200

    Updates

---
+++ precache-manifest.579f6fae93da8923b57ff348f8964ab4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2910671cb0da3ae30dd4",
+    "url": "/react-vod-app/static/js/runtime~main.00b92041.js"
+  },
+  {
+    "revision": "41e17271201412421ed1",
+    "url": "/react-vod-app/static/js/main.efafde9f.chunk.js"
+  },
+  {
+    "revision": "5ef4610432222d77aef7",
+    "url": "/react-vod-app/static/js/2.045d7a79.chunk.js"
+  },
+  {
+    "revision": "41e17271201412421ed1",
+    "url": "/react-vod-app/static/css/main.a4d0989a.chunk.css"
+  },
+  {
+    "revision": "1ab4dfd2d5b74672435290a8ce046b58",
+    "url": "/react-vod-app/index.html"
+  }
+];
